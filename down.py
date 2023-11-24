5c0a4623410734b3ded270cd86601608574f2fe5
Louka Ewington-Pitsos <lewingtonpitsos@gmail.com>
2023-11-24 11:06:17 +1100

    down

--- down.py
+++ down.py
@@ -1,10 +1,11 @@
+import numpy as np
 from matplotlib import pyplot as plt
 import climetlab as cml	
 
 ds = cml.load_source(
 	"cds",
 	"reanalysis-era5-single-levels",
-	param=["2t", "msl"],
+	param=["2t"],
 	product_type="reanalysis",
 	grid='5/5',
 	date=["2012-12-12", "2012-12-13"],
@@ -13,10 +14,8 @@ ds = cml.load_source(
 
 ds = ds.to_xarray()
 ds = ds.isel(time=0)
-print(ds.dims)
 
-print(ds.variables)
-print(type(ds.msl.values))
-
-plt.imshow(ds.msl.values.squeeze())
+print(dir(ds))
+print(ds.var)
+plt.imshow(ds.t2m.squeeze())
 plt.savefig('cruft/era5.png')
