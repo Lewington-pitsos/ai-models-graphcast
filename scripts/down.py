ef0cc72185b66c3c84931cefa7b39583ce019894
Louka <lewingtonpitsos@gmail.com>
2024-01-14 06:31:01 +0000

    get analysis code working

--- scripts/down.py
+++ scripts/down.py
@@ -4,16 +4,10 @@ import datetime
 from statelist import StateList
 import xarray
 
-# p = StateList(datetime.datetime(2023, 11, 17, 6), 91, 6)
-# predicted_dates = p.dates_mapped_to_hours(
-# 	upper_bound=datetime.datetime(2023, 11, 19, 3)
-# )
-
 p = StateList(datetime.datetime(2023, 12, 22, 12), 241, 6)
 predicted_dates = p.dates_mapped_to_hours(
 	upper_bound=datetime.datetime(2024, 1, 7, 23)
 )
-
 
 print('dates to download', predicted_dates)
 
@@ -30,20 +24,8 @@ for date, hours in predicted_dates:
 	)
 	all_ds.append(ds)
 
-print(all_ds)
-
 filename = 'cruft/20231222-era5.nc'
 os.makedirs(os.path.dirname(filename), exist_ok=True)
 
 ds = xarray.concat([d.to_xarray() for d in all_ds], dim='time')
 ds.to_netcdf(filename)
-
-print(ds)
-
-# ds = ds.to_xarray()
-# ds = ds.isel(time=0)
-
-# print(dir(ds))
-# print(ds.var)
-# plt.imshow(ds.t2m.squeeze())
-# plt.savefig('cruft/era5.png')
--- scripts/om.py
+++ scripts/om.py
@@ -9,46 +9,64 @@ def get_forcast(openmeteo, params):
 
 	responses = openmeteo.weather_api(url, params=params)
 
-	# Process first location. Add a for-loop for multiple locations or weather models
-	response = responses[0]
+	all_response_dfs = []
+	for response in responses:
 
-	# Process hourly data. The order of variables needs to be the same as requested.
-	hourly = response.Hourly()
-	hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+		# Process hourly data. The order of variables needs to be the same as requested.
+		hourly = response.Hourly()
+		hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
 
-	hourly_data = {"date": pd.date_range(
-		start = pd.to_datetime(hourly.Time(), unit = "s"),
-		end = pd.to_datetime(hourly.TimeEnd(), unit = "s"),
-		freq = pd.Timedelta(seconds = hourly.Interval()),
-		inclusive = "left"
-	)}
-	hourly_data["temperature_2m"] = hourly_temperature_2m
+		hourly_data = {"date": pd.date_range(
+			start = pd.to_datetime(hourly.Time(), unit = "s"),
+			end = pd.to_datetime(hourly.TimeEnd(), unit = "s"),
+			freq = pd.Timedelta(seconds = hourly.Interval()),
+			inclusive = "left"
+		)}
+		hourly_data["temperature_2m"] = hourly_temperature_2m
+		hourly_data["latitude"] = response.Latitude()
+		hourly_data["longitude"] = response.Longitude()
+		idx = response.LocationId()
+		hourly_data["requested_lat"] = params['latitude'][idx]
+		hourly_data["requested_lon"] = params['longitude'][idx]
 
-	hourly_dataframe = pd.DataFrame(data = hourly_data)
-	return hourly_dataframe
+
+		hourly_dataframe = pd.DataFrame(data = hourly_data)
+		# print(hourly_dataframe)
+		all_response_dfs.append(hourly_dataframe)
+		
+	return pd.concat(all_response_dfs)
 
 def get_reanalysis(openmeteo, params):
 	url = "https://archive-api.open-meteo.com/v1/archive"
 	responses = openmeteo.weather_api(url, params=params)
 
 	# Process first location. Add a for-loop for multiple locations or weather models
-	response = responses[0]
+	all_response_dfs = []
+	for response in responses:
 
-	# Process hourly data. The order of variables needs to be the same as requested.
-	hourly = response.Hourly()
-	hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+		# Process hourly data. The order of variables needs to be the same as requested.
+		hourly = response.Hourly()
+		hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
 
-	hourly_data = {"date": pd.date_range(
-		start = pd.to_datetime(hourly.Time(), unit = "s"),
-		end = pd.to_datetime(hourly.TimeEnd(), unit = "s"),
-		freq = pd.Timedelta(seconds = hourly.Interval()),
-		inclusive = "left"
-	)}
-	hourly_data["temperature_2m"] = hourly_temperature_2m
+		hourly_data = {"date": pd.date_range(
+			start = pd.to_datetime(hourly.Time(), unit = "s"),
+			end = pd.to_datetime(hourly.TimeEnd(), unit = "s"),
+			freq = pd.Timedelta(seconds = hourly.Interval()),
+			inclusive = "left"
+		)}
+		hourly_data["temperature_2m"] = hourly_temperature_2m
+		hourly_data["latitude"] = response.Latitude()
+		hourly_data["longitude"] = response.Longitude()
+		idx = response.LocationId()
+		hourly_data["requested_lat"] = params['latitude'][idx]
+		hourly_data["requested_lon"] = params['longitude'][idx]
 
-	hourly_dataframe = pd.DataFrame(data = hourly_data)
 
-	return hourly_dataframe 
+		hourly_dataframe = pd.DataFrame(data = hourly_data)
+		print(hourly_dataframe)
+		all_response_dfs.append(hourly_dataframe)
+
+	return pd.concat(all_response_dfs)
 
 def get_om_data(params):
 	# Setup the Open-Meteo API client with cache and retry on error
@@ -60,16 +78,15 @@ def get_om_data(params):
 	print('downloaded forcast')
 	re = get_reanalysis(openmeteo, params)
 	print('downloaded reanalysis')
-	df = pd.merge(re, fo, on='date', how='outer', suffixes=['_reanalysis', '_forcast'])
+	df = pd.merge(re, fo, on=['date', 'requested_lon', 'requested_lat'], how='outer', suffixes=['_reanalysis', '_forcast'])
 
 	return df
 
 start_date = '2023-12-22'
 end_date = '2024-02-07'
-param_name = f'{start_date}-{end_date}'
+param_name = f'{start_date}-{end_date}-melbgrid'
 
 params = {
-	# grid around melbourne
 	"latitude": [-38.5 , -38.5 , -38.5 , -38.5 , -38.5 , -38.5 , -38.5 , -38.25,
        -38.25, -38.25, -38.25, -38.25, -38.25, -38.25, -38.  , -38.  ,
        -38.  , -38.  , -38.  , -38.  , -38.  , -37.75, -37.75, -37.75,
@@ -84,6 +101,9 @@ params = {
        145.25, 145.5 , 145.75, 144.25, 144.5 , 144.75, 145.  , 145.25,
        145.5 , 145.75, 144.25, 144.5 , 144.75, 145.  , 145.25, 145.5 ,
        145.75],
+
+	# "latitude": [-37.75],
+	# "longitude": [145.0],
 	"hourly": "temperature_2m",
 	"start_date": "2023-12-22",
 	"end_date": "2024-01-01",
